061721203c3b7cca054dd8a562be5450c843e4f9
mythicmilo <mythicalmilo@hotmail.com>
2026-03-23 14:32:26 -0400

    Implemented all game functionality

--- src/BattleshipBoard.java
+++ src/BattleshipBoard.java
@@ -1,37 +1,227 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class BattleshipBoard
 {
     private BattleshipTile[][] board;
     private String[][] shipBoard;
-    private ImageIcon icon;
+    private ImageIcon oceanIcon;
+    private ImageIcon splashIcon;
+    private ImageIcon explosionIcon;
+    private int missCount = 0;
+    private int totalMissCount = 0;
+    private int strikeCount = 0;
+    private int totalHitCount = 0;
+    private int shipCount = 1;
+    private int sunkCount = 0;
+    private ArrayList<Integer> ship1;
+    private ArrayList<Integer> ship2;
+    private ArrayList<Integer> ship3;
+    private ArrayList<Integer> ship4;
+    private ArrayList<Integer> ship5;
     private final int[] ships = {5, 4, 3, 3, 2};
-    private final int ROW = 10;
-    private final int COL = 10;
+    private final int ROWS = 10;
+    private final int COLS = 10;
 
-    public BattleshipBoard(JPanel boardPnl)
+    public BattleshipBoard(JPanel boardPnl, JTextField totalHitTF,
+                           JTextField totalMissTF, JTextField missTF,
+                           JTextField strikeTF)
     {
-        board = new BattleshipTile[ROW][COL];
-        icon = new ImageIcon("src/ocean.jpg");
+        board = new BattleshipTile[ROWS][COLS];
+        oceanIcon = new ImageIcon("src/ocean.jpg");
         ActionListener tileListener = new ActionListener()
         {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-
+                Object tile = e.getSource();
+                checkShipBoard(tile, totalHitTF, totalMissTF, missTF, strikeTF);
             }
         };
-        for (int row = 0; row < 10; row++)
+        for (int row = 0; row < ROWS; row++)
         {
-            for (int col = 0; col < 10; col++)
+            for (int col = 0; col < COLS; col++)
             {
                 board[row][col] = new BattleshipTile(row, col);
-                board[row][col].setIcon(icon);
+                board[row][col].setIcon(oceanIcon);
                 board[row][col].addActionListener(tileListener);
                 boardPnl.add(board[row][col]);
+            }
+        }
+        placeShips();
+    }
+
+    public void resetGame()
+    {
+        for (int row = 0; row < ROWS; row++)
+        {
+            for (int col = 0; col < COLS; col++)
+            {
+                board[row][col] = new BattleshipTile(row, col);
+                board[row][col].setIcon(oceanIcon);
+                board[row][col].setEnabled(true);
+            }
+        }
+        missCount = 0;
+        totalMissCount = 0;
+        strikeCount = 0;
+        totalHitCount = 0;
+        shipCount = 1;
+    }
+
+    public void checkShipBoard(Object tile, JTextField totalHitTF,
+                               JTextField totalMissTF, JTextField missTF,
+                               JTextField strikeTF)
+    {
+        splashIcon = new ImageIcon("src/splash.jpg");
+        explosionIcon = new ImageIcon("src/explosion.jpg");
+
+        for (int row = 0; row < ROWS; row++)
+        {
+            for (int col = 0; col < COLS; col++)
+            {
+                if (tile == board[row][col])
+                {
+                    if (shipBoard[row][col].equals("placed"))
+                    {
+                        shipBoard[row][col] = "hit";
+                        board[row][col].setIcon(explosionIcon);
+                        totalHitCount++;
+                        totalHitTF.setText("" + totalHitCount);
+                        missCount = 0;
+                        missTF.setText("" + missCount);
+                        checkShipStatus(row, col);
+                    }
+                    else
+                    {
+                        board[row][col].setIcon(splashIcon);
+                        totalMissCount++;
+                        totalMissTF.setText("" + totalMissCount);
+                        missCount++;
+                        missTF.setText("" + missCount);
+                        if (missCount == 5)
+                        {
+                            strikeCount++;
+                            strikeTF.setText("" + strikeCount);
+                            missCount = 0;
+                            checkStrike();
+                        }
+                    }
+                    board[row][col].setEnabled(false);
+                }
+            }
+        }
+    }
+
+    public void checkShipStatus(int row, int col)
+    {
+        int hitCount = 1;
+        if (ship1.contains(row) && ship1.contains(col))
+        {
+            for (int i = 1; i < ship1.size(); i++)
+            {
+                if (shipBoard[ship1.getFirst()][ship1.get(i)].equals("hit"))
+                {
+                    hitCount++;
+                }
+            }
+            if (hitCount == ship1.size())
+            {
+                sunkCount++;
+                JOptionPane.showMessageDialog(null, "The ship has sunk!");
+                checkWin();
+            }
+        }
+        else if (ship2.contains(row) && ship2.contains(col))
+        {
+            for (int i = 1; i < ship2.size(); i++)
+            {
+                if (shipBoard[ship2.getFirst()][ship2.get(i)].equals("hit"))
+                {
+                    hitCount++;
+                }
+            }
+            if (hitCount == ship2.size())
+            {
+                sunkCount++;
+                JOptionPane.showMessageDialog(null, "The ship has sunk!");
+                checkWin();
+            }
+        }
+        else if (ship3.contains(row) && ship3.contains(col))
+        {
+            for (int i = 1; i < ship3.size(); i++)
+            {
+                if (shipBoard[ship3.getFirst()][ship3.get(i)].equals("hit"))
+                {
+                    hitCount++;
+                }
+            }
+            if (hitCount == ship3.size())
+            {
+                sunkCount++;
+                JOptionPane.showMessageDialog(null, "The ship has sunk!");
+                checkWin();
+            }
+        }
+        else if (ship4.contains(row) && ship4.contains(col))
+        {
+            for (int i = 1; i < ship4.size(); i++)
+            {
+                if (shipBoard[ship4.getFirst()][ship4.get(i)].equals("hit"))
+                {
+                    hitCount++;
+                }
+            }
+            if (hitCount == ship4.size())
+            {
+                sunkCount++;
+                JOptionPane.showMessageDialog(null, "The ship has sunk!");
+                checkWin();
+            }
+        }
+        else if (ship5.contains(row) && ship5.contains(col))
+        {
+            for (int i = 1; i < ship5.size(); i++)
+            {
+                if (shipBoard[ship5.getFirst()][ship5.get(i)].equals("hit"))
+                {
+                    hitCount++;
+                }
+            }
+            if (hitCount == ship5.size())
+            {
+                sunkCount++;
+                JOptionPane.showMessageDialog(null, "The ship has sunk!");
+                checkWin();
+            }
+        }
+    }
+    public void checkWin()
+    {
+        if (sunkCount == shipCount)
+        {
+            JOptionPane.showMessageDialog(null, "You won!");
+            int playAgain = JOptionPane.showConfirmDialog(null, "Do you want to play again?", "Confirm Play Again", JOptionPane.YES_NO_OPTION);
+            if (playAgain == JOptionPane.YES_OPTION)
+            {
+                resetGame();
+            }
+        }
+    }
+
+    public void checkStrike()
+    {
+        if (strikeCount == 3)
+        {
+            JOptionPane.showMessageDialog(null, "You Lost!");
+            int playAgain = JOptionPane.showConfirmDialog(null, "Do you want to play again?", "Confirm Play Again", JOptionPane.YES_NO_OPTION);
+            if (playAgain == JOptionPane.YES_OPTION)
+            {
+                resetGame();
             }
         }
     }
@@ -65,9 +255,9 @@ public class BattleshipBoard
 
     private void emptyShipBoard()
     {
-        for (int row = 0; row < 10; row++)
+        for (int row = 0; row < ROWS; row++)
         {
-            for (int col = 0; col < 10; col++)
+            for (int col = 0; col < COLS; col++)
             {
                 shipBoard[row][col] = "empty";
             }
@@ -110,6 +300,56 @@ public class BattleshipBoard
                 return false;
             }
         }
+        if (shipCount == 1)
+        {
+            ship1.add(row);
+            for (int j = 0; j < shipLength; j++)
+            {
+                int col = colStart + j;
+                ship1.add(col);
+            }
+            shipCount++;
+        }
+        else if (shipCount == 2)
+        {
+            ship2.add(row);
+            for (int j = 0; j < shipLength; j++)
+            {
+                int col = colStart + j;
+                ship2.add(col);
+            }
+            shipCount++;
+        }
+        else if (shipCount == 3)
+        {
+            ship3.add(row);
+            for (int j = 0; j < shipLength; j++)
+            {
+                int col = colStart + j;
+                ship3.add(col);
+            }
+            shipCount++;
+        }
+        else if (shipCount == 4)
+        {
+            ship4.add(row);
+            for (int j = 0; j < shipLength; j++)
+            {
+                int col = colStart + j;
+                ship4.add(col);
+            }
+            shipCount++;
+        }
+        else
+        {
+            ship5.add(row);
+            for (int j = 0; j < shipLength; j++)
+            {
+                int col = colStart + j;
+                ship5.add(col);
+            }
+            shipCount++;
+        }
         return true;
     }
 
@@ -126,6 +366,56 @@ public class BattleshipBoard
                 resetColPlacement(col, rowStart, shipLength);
                 return false;
             }
+        }
+        if (shipCount == 1)
+        {
+            ship1.add(col);
+            for (int j = 0; j < shipLength; j++)
+            {
+                int row = rowStart + j;
+                ship1.add(row);
+            }
+            shipCount++;
+        }
+        else if (shipCount == 2)
+        {
+            ship2.add(col);
+            for (int j = 0; j < shipLength; j++)
+            {
+                int row = rowStart + j;
+                ship2.add(row);
+            }
+            shipCount++;
+        }
+        else if (shipCount == 3)
+        {
+            ship3.add(col);
+            for (int j = 0; j < shipLength; j++)
+            {
+                int row = rowStart + j;
+                ship3.add(row);
+            }
+            shipCount++;
+        }
+        else if (shipCount == 4)
+        {
+            ship4.add(col);
+            for (int j = 0; j < shipLength; j++)
+            {
+                int row = rowStart + j;
+                ship4.add(row);
+            }
+            shipCount++;
+        }
+        else
+        {
+            ship5.add(col);
+            for (int j = 0; j < shipLength; j++)
+            {
+                int row = rowStart + j;
+                ship5.add(row);
+            }
+            shipCount++;
         }
         return true;
     }
